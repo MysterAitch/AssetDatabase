c4cf8ba3cbb8d66edfae00ecfd2c72d3603e21ed
Roger Howell <MysterAitch@users.noreply.github.com>
2023-08-29 23:30:05 +0100

    Add "real" / functional unit test and fix implementation of temperature conversion between C->F

---
+++ AssetDatabase/AssetDatabase.API.Test/WeatherForecastTest.cs
@@ -0,0 +1,18 @@
+namespace AssetDatabase.API.Test;
+
+public class WeatherForecastTest
+{
+    [Theory]
+    [InlineData(0, 32)]
+    [InlineData(100, 212)]
+    [InlineData(-40, -40)]
+    public void CelsiusToFahrenheitTheory(int celsius, int fahrenheit)
+    {
+        var forecastModel = new WeatherForecast()
+        {
+            TemperatureC = celsius,
+        };
+        
+        Assert.Equal(fahrenheit, forecastModel.TemperatureF);
+    }
+}
